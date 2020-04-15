41b885ab24978049a76fd386c4233a23f1759fc4
Douglas Wu <wckdouglas@gmail.com>
2020-04-15 08:17:30 -0400

    used bokeh

--- src/utils.py
+++ src/utils.py
@@ -1,45 +1,33 @@
 import pandas as pd
-<<<<<<< HEAD
 import geopandas as gpd
-=======
->>>>>>> 860ea881f47a828b8cb4b8fca8356c1f34ff2575
 import glob
 import os
+from bs4 import BeautifulSoup
+import requests
 
 class Data():
     def __init__(self, state = 'MD'):
         #data and URL path
-<<<<<<< HEAD
         self.state = state
-=======
->>>>>>> 860ea881f47a828b8cb4b8fca8356c1f34ff2575
         self.data_path = 'data/'
         self.zip_map_url = 'https://public.opendatasoft.com/explore/dataset/us-zip-code-latitude-and-longitude'\
                 '/download/?format=csv&timezone=America/New_York'\
                 '&lang=en&use_labels_for_header=true&csv_separator=%3B'
-<<<<<<< HEAD
-        #geo shape: https://catalog.data.gov/dataset/tiger-line-shapefile-2016-2010-nation-u-s-2010-census-5-digit-zip-code-tabulation-area-zcta5-na
+        self.population_url = 'https://www.maryland-demographics.com/zip_codes_by_population'
 
         #actual reading dat
         self.geo = self.read_map()
         self.zip_map = self.read_zip_map()
         self.zip_codes = self.zip_map.Zip
         self.zip_covid = self.read_zip_COVID()
+        self.zip_population = self.read_population()
         
     def read_map(self):
-        return gpd.read_file('{}/tl_2016_us_zcta510.shp'.format(self.data_path)) \
+        #geo shape: https://catalog.data.gov/dataset/tiger-line-shapefile-2016-2010-nation-u-s-2010-census-5-digit-zip-code-tabulation-area-zcta5-na
+        return gpd.read_file('zip://{}/tl_2019_us_zcta510.zip'.format(self.data_path)) \
             .rename(columns = {'ZCTA5CE10':'Zip'}) \
             .assign(Zip = lambda d: d.Zip.astype(int))
         
-=======
-        self.zip_census = 'https://data.lacity.org/api/views/nxs9-385f/rows.csv?accessType=DOWNLOAD'
-
-        #actual reading dat
-        self.zip_map = self.read_zip_map()
-        self.zip_codes = self.zip_map.Zip
-        self.zip_covid = self.read_zip_COVID()
-        self.zip_census = self.read_zip_stat()
->>>>>>> 860ea881f47a828b8cb4b8fca8356c1f34ff2575
 
     def read_zip_COVID(self):
         covid_data = {}
@@ -56,11 +44,23 @@ class Data():
         return pd.read_csv(self.zip_map_url, sep=';') \
             .query('State == "%s"' %self.state)
 
-<<<<<<< HEAD
-=======
-
-    def read_zip_stat(self):
-        return pd.read_csv(self.zip_census) \
-            .rename(columns = {'Zip Code':'Zip'}) \
-            .pipe(lambda d: d[d.Zip.isin(self.zip_codes)])
->>>>>>> 860ea881f47a828b8cb4b8fca8356c1f34ff2575
+    def read_population(self):
+        http = requests.get(self.population_url)
+        soup = BeautifulSoup(http.content)
+        table = soup.find_all('table')
+        table = pd.read_html(str(table))[0] \
+            .filter(['Zip Code','Population'])\
+            .rename(columns = {'Zip Code':'Zip'})\
+            .iloc[:-1,:]
+        rows = []
+        for i, row in table.iterrows():
+            row_dict = {}
+            if 'and' not in row['Zip']:
+                row_dict['Zip'] = row['Zip']
+                row_dict['Population'] = row['Population']
+            else:
+                for zip_code in row['Zip'].split(' and '):
+                    row_dict['Zip'] = zip_code
+                    row_dict['Population'] = int(row['Population'])/2
+            rows.append(row_dict)
+        return pd.DataFrame(rows)
