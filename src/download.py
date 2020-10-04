6543827db272ede664e1ae8c6fa3840d4d8b9e57
Douglas Wu <wckdouglas@gmail.com>
2020-10-04 10:45:36 -0400

    added download

--- src/download.py
+++ src/download.py
@@ -7,25 +7,28 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('Get')
 
 def parse_date(date):
-	date_regex = re.compile('20[0-9]{2}-[0-9]{2}\-[0-9]{2}')
-	if date_regex.search(date):
-		year, month, day = date.split('-')
-	else:
-		raise ValueError('Date parameter must be in the format of YYYY-MM-DD')
-	return 'total{M}_{D}_{Y}'.format(M = month, D = day, Y = year)
+    date_regex = re.compile('20[0-9]{2}-[0-9]{2}\-[0-9]{2}')
+    if date_regex.search(date):
+        year, month, day = date.split('-')
+        logger.info('Getting Year: %s; Month: %s; Day: %s' %(year, month, day))
+    else:
+        raise ValueError('Date parameter must be in the format of YYYY-MM-DD')
+    return 'total{M}_{D}_{Y}'.format(M = month, D = day, Y = year)
 
 
 
 def get(date):
-	'''
-	Select data from one day and output in the data format
-	'''
-	logger.info('Downloading %s' %date)
-	date = parse_date(date)
-	logger.info('Using column %s' %date)
-	gpd.read_file(Data().MD_zip_data_url)\
-		.filter(['ZIP_CODE',date])\
-		.fillna(0)\
-		.query('%s>0' %date)\
-		.assign(**{date: lambda d: d[date].astype(int).astype(str) + ' Cases'})\
-		.to_csv(sys.stdout,sep='\t', index=False, header=False)
+    '''
+    Select data from one day and output in the data format
+    '''
+    logger.info('Downloading %s' %date)
+    date = parse_date(date)
+    logger.info('Using column %s' %date)
+    db = gpd.read_file(Data().MD_zip_data_url)
+    if date not in db.columns:
+        raise IndexError('Column %s not in database' %date)
+    db.filter(['ZIP_CODE',date])\
+        .fillna(0)\
+        .query('%s>0' %date)\
+        .assign(**{date: lambda d: d[date].astype(int).astype(str) + ' Cases'})\
+        .to_csv(sys.stdout,sep='\t', index=False, header=False)
