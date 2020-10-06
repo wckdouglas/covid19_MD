1172b405464b9a109371a5edb8c32e0b609d6292
Douglas Wu <wckdouglas@gmail.com>
2020-10-06 13:27:33 -0400

    added 10/5

--- src/utils.py
+++ src/utils.py
@@ -194,7 +194,7 @@ def get_data(ts_data_file = '../data/ts.csv', map_data_file = '../data/MD.geojso
         .pipe(lambda d: d[d.Date==d.Date.max()]) \
         .assign(Cases = lambda d: d.Cases.fillna(0)) \
         .pipe(lambda d: d[~pd.isnull(d.geometry)]) \
-        .assign(per_population = lambda d: d.Cases / d.Population.astype(int) * 1e6)
+        .assign(per_population = lambda d: d.Cases *1e6 / d.Population.astype(int) )
     today = str(total_case_data.Date.astype(str).unique()[0])
 
     per_day_increase_data =  data \
@@ -205,7 +205,7 @@ def get_data(ts_data_file = '../data/ts.csv', map_data_file = '../data/MD.geojso
                         .assign(increase = lambda d: d.increase.fillna(0))\
                         .pipe(lambda d: d[d.Date == d.Date.max()])) \
         .pipe(lambda d: d[~pd.isnull(d.geometry)]) \
-        .assign(per_population = lambda d: d.increase / d.Population.astype(int) * 1e6) \
+        .assign(per_population = lambda d: d.increase *1e6 / d.Population.astype(int) ) \
         .assign(Date = lambda d: d.Date.astype(str))
 
     ts_data = maryland.zip_covid\
