41c2dceec13799e1720beb4a4d0f99ecec53c421
Douglas Wu <wckdouglas@gmail.com>
2021-02-04 18:36:44 -0500

    fixed test

--- test/test_update.py
+++ test/test_update.py
@@ -11,7 +11,7 @@ class run_args():
     def __init__(self, use_db = False, refresh = False):
         self.refresh = refresh
         self.out_html = './out.html'
-        self.datadir = os.path.dirname(os.path.abspath(__file__)) + '../data'
+        self.datadir = os.path.dirname(os.path.abspath(__file__)) + '/../data'
 
 @pytest.fixture
 def input():
