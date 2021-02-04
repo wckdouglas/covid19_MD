cbbdce86da57be5c314e36b3b8bdbd74a17f11f8
Douglas Wu <wckdouglas@gmail.com>
2021-02-04 18:48:47 -0500

    added 2021-02-04

--- update_today.sh
+++ update_today.sh
@@ -7,14 +7,14 @@ echo Running $TODAY
 #Fetch data
 cd $COVID_REPO
 git pull
-docker run md_covid19 get --date $TODAY > data/${TODAY}.tsv
+docker run wckdouglas/md_covid19 get --date $TODAY > data/${TODAY}.tsv
 echo Fetched $TODAY
 git add data/${TODAY}.tsv
 git commit -am "added ${TODAY}"
 git push
 
 # generate dashboard
-docker run -v "$(pwd):/data" md_covid19 update -o /data/dashboard.html --datadir /data/data
+docker run -v "$(pwd):/data" wckdouglas/md_covid19 update -o /data/dashboard.html --datadir /data/data
 cd $WEB_REPO
 git pull
 cat $COVID_REPO/dashboard.html | sed 's/<!DOCTYPE html>//g' > $WEB_REPO/_includes/COVID.html
