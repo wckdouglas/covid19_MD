1218efe4c57ccdf487b93ae90c2c4eecff3378fe
Douglas Wu <wckdouglas@gmail.com>
2021-03-24 20:06:18 -0400

    updated refresh code

--- update_today.sh
+++ update_today.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -v -x
+BIN_PATH=/Users/wckdouglas/miniconda3/bin
 COVID_REPO=/Users/wckdouglas/codes/covid19_MD
 WEB_REPO=/Users/wckdouglas/codes/wckdouglas.github.io
 TODAY=$(date +"%Y-%m-%d")
@@ -7,14 +8,14 @@ echo Running $TODAY
 #Fetch data
 cd $COVID_REPO
 git pull
-poetry run python dashboard.py get --date $TODAY > data/${TODAY}.tsv
+$BIN_PATH/poetry run python dashboard.py get --date $TODAY > data/${TODAY}.tsv
 echo Fetched $TODAY
 git add data/${TODAY}.tsv
 git commit -am "added ${TODAY}"
 git push
 
 # generate dashboard
-poetry run python dashboard.py update -o dashboard.html --datadir data
+$BIN_PATH/poetry run python dashboard.py update -o dashboard.html --datadir data
 cd $WEB_REPO
 git pull
 cat $COVID_REPO/dashboard.html | sed 's/<!DOCTYPE html>//g' > $WEB_REPO/_includes/COVID.html
